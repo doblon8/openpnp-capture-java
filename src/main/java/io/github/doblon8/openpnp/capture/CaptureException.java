3325822253db4a1340697bae5220d632e3a7cfcd
doblon8 <doblon8@proton.me>
2026-06-01 10:31:40 +0200

    Add CaptureException

---
+++ src/main/java/io/github/doblon8/openpnp/capture/CaptureException.java
@@ -0,0 +1,7 @@
+package io.github.doblon8.openpnp.capture;
+
+public class CaptureException extends RuntimeException {
+    public CaptureException(String message) {
+        super(message);
+    }
+}
