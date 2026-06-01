681312d11706511a37824944f123c5dd15f2f68f
doblon8 <doblon8@proton.me>
2026-06-01 10:31:39 +0200

    Add CaptureContext class for context management

---
+++ src/main/java/io/github/doblon8/openpnp/capture/CaptureContext.java
@@ -0,0 +1,19 @@
+package io.github.doblon8.openpnp.capture;
+
+import java.lang.foreign.MemorySegment;
+
+import static io.github.doblon8.openpnp.capture.bindings.openpnp_capture.Cap_createContext;
+import static io.github.doblon8.openpnp.capture.bindings.openpnp_capture.Cap_releaseContext;
+
+public final class CaptureContext {
+
+    private final MemorySegment segment;
+
+    public CaptureContext() {
+        segment = Cap_createContext();
+    }
+
+    public int releaseContext() {
+        return Cap_releaseContext(segment);
+    }
+}
