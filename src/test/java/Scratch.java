cc7ad693106c894ed6fa189461406b1783fc36d6
doblon8 <doblon8@proton.me>
2026-06-01 10:31:40 +0200

    Use a custom log function and create a capture context in the example

--- src/test/java/Scratch.java
+++ src/test/java/Scratch.java
@@ -1,6 +1,17 @@
+import io.github.doblon8.openpnp.capture.CaptureContext;
+import io.github.doblon8.openpnp.capture.LogLevel;
 import io.github.doblon8.openpnp.capture.OpenPnpCapture;
+
+import static io.github.doblon8.openpnp.capture.OpenPnpCapture.installCustomLogFunction;
+import static io.github.doblon8.openpnp.capture.OpenPnpCapture.setLogLevel;
 
 void main() {
     var version = OpenPnpCapture.getLibraryVersion();
     System.out.println("OpenPnP Capture Library Version: " + version);
+
+    setLogLevel(LogLevel.VERBOSE);
+    installCustomLogFunction((logLevel, message) -> System.out.println("Custom Log [" + logLevel + "]: " + message));
+
+    var context = new CaptureContext();
+    var result = context.releaseContext();
 }
