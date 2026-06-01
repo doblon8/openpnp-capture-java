c28ff6c82370683811aa218bd4fd2c368d9a1ec1
doblon8 <doblon8@proton.me>
2026-06-01 10:31:33 +0200

    Initialize project

---
+++ src/main/java/io/github/doblon8/openpnp/capture/Main.java
@@ -0,0 +1,7 @@
+package io.github.doblon8.openpnp.capture;
+
+public class Main {
+    static void main() {
+        System.out.println("Java Foreign Function & Memory bindings for openpnp-capture");
+    }
+}
